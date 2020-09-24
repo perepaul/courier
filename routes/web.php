423c1dead7651427a71fd15792ede8d7d097d489
= <=>
2020-09-24 20:15:15 +0100

    asigned links

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-Route::get('test',function() {
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+Route::get('admin',function() {
     return view('admin.index');
 });
-Route::get('front', function () {
+Route::get('/', function () {
     return view('layouts.front');
 });
 
